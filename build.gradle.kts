42177e57ed0d25460f092cbf22fed8213c0ad232
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-04-02 16:46:30 +0200

    Add memory to jvm when run jpf test

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ File(rootProject.rootDir.path + searchingPath).listFiles()
                         }
                 )
                 mainClass.set("-jar")
-                args = listOf("-Xmx2G", "./jpf-runner/build/RunJPF.jar", ".${searchingPath}" + file.name)
+                args = listOf("-Xmx4G", "./jpf-runner/build/RunJPF.jar", ".${searchingPath}" + file.name)
             }
             val capitalizedName = it.nameWithoutExtension.capitalize()
             val jpfVerification by launchVerificationTask("run${capitalizedName}Verify", it)
